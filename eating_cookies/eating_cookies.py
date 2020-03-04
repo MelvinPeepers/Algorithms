f6549c251b73523972d42fb386758d8382e83a92
Melvin <peepersm@gmail.com>
2020-03-04 18:30:14 -0500

    eating_cookies.py tested

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,6 +6,76 @@ import sys
 # a solution that is more efficient than the naive
 # recursive solution
 
+# Cookies.       Ways to eat
+
+# 0
+# —————
+# 1
+
+# 1
+# —————
+# 1
+
+# 2
+# —————
+# 1,1
+# 2
+
+# 3
+# —————
+# 1, 1, 1
+# 1, 2
+# 2, 1
+# 3
+
+# # Look at the ways to eat (n-1) cookies, then eat one more
+# # Now look at the ways to eat (n-2) cookies, then eat 2 at once
+# # Now look at the ways to eat (n-3) cookies, then eat 3 at once
+# # Now look at the ways to eat (n-4) cookies, then eat 4 at once
+
+# 4
+# —————
+# 1, 1, 1, 1
+# 1, 1, 2
+# 1, 2, 1
+# 2, 1, 1
+# 2, 2
+# 1, 3
+# 3, 1
+
+
+# # Look at the ways to eat (n-1) cookies, then eat one more
+# # Now look at the ways to eat (n-2) cookies, then eat 2 at once
+# # Now look at the ways to eat (n-3) cookies, then eat 3 at once
+# # Now look at the ways to eat (n-4) cookies, then eat 4 at once
+# # Now look at the ways to eat (n-5) cookies, then eat 5 at once
+
+# 5
+# —————
+# 1, 1, 1, 1, 1
+# 1, 1, 2, 1
+# 1, 2, 1, 1
+# 2, 1, 1, 1
+# 2, 2, 1
+# 1, 3, 1
+# 3, 1, 1
+# 1, 1, 1, 2
+# 1, 2, 2
+# 2, 1, 2
+# 3, 2
+# 1,1, 3
+# 2, 3
+
+# Plan
+# How to turn this into code?
+# # Find all combinations of 1, 2, 3 that add up to n
+# # Find all possible orderings of those combinations
+# 1, 1, 1, 1, 1
+# 1, 1, 1, 2
+# 1, 1, 3
+# 1, 2, 2
+# 2, 3
+
 
 def eating_cookies(n, cache=None):
     if n < 0:
@@ -18,7 +88,7 @@ def eating_cookies(n, cache=None):
         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 
-print(eating_cookies(5))
+# print(eating_cookies(10))
 
 
 if __name__ == "__main__":
